a808695a2278f2fb2572ed3f53d0e30ee960c5be
Fr3myXS <antonovch13@mail.ru>
2021-05-05 18:35:10 +0500

    Update.Logic 4

--- Chess_Game.Logic/Info.cs
+++ Chess_Game.Logic/Info.cs
@@ -12,13 +12,13 @@ namespace Chess_Game.Logic
     {
         public static void SaveGame(string namePlayerOne, string namePlayerTwo)
         {
-            Directory.CreateDirectory($"data/{namePlayerOne}_{namePlayerTwo}");
+            Directory.CreateDirectory($"data/Saves/{namePlayerOne}_{namePlayerTwo}");
             string serialized = JsonConvert.SerializeObject(BoardModel.Board, Formatting.Indented);
-            File.WriteAllText($"data/{namePlayerOne}_{namePlayerTwo}/board.txt", serialized);
+            File.WriteAllText($"data/Saves/{namePlayerOne}_{namePlayerTwo}/board.txt", serialized);
         }
         public static void LoadGame(string nameOne, string nameTwo)
         {
-            string txtInfo = File.ReadAllText($"data/{nameOne}_{nameTwo}/board.txt");
+            string txtInfo = File.ReadAllText($"data/Saves/{nameOne}_{nameTwo}/board.txt");
             var tes = JsonConvert.DeserializeObject<Figure[,]>(txtInfo);
             BoardModel.Board = tes;
 
@@ -28,14 +28,14 @@ namespace Chess_Game.Logic
         public static void SavePlayers(Player playerOne, Player playerTwo)
         {
             string platerOneInfo = JsonConvert.SerializeObject(playerOne, Formatting.Indented);
-            File.WriteAllText($"data/{playerOne.Name}_{playerTwo.Name}/playerOne.txt", platerOneInfo);
+            File.WriteAllText($"data/Saves/{playerOne.Name}_{playerTwo.Name}/playerOne.txt", platerOneInfo);
 
             string platerTwoInfo = JsonConvert.SerializeObject(playerTwo, Formatting.Indented);
-            File.WriteAllText($"data/{playerOne.Name}_{playerTwo.Name}/playerTwo.txt", platerTwoInfo);
+            File.WriteAllText($"data/Saves/{playerOne.Name}_{playerTwo.Name}/playerTwo.txt", platerTwoInfo);
         }
         private static Player LoadPlayers(string nameOne, string nameTwo, string name = "playerOne")
         {
-            string txtInfo = File.ReadAllText($"data/{nameOne}_{nameTwo}/{name}.txt");
+            string txtInfo = File.ReadAllText($"data/Saves/{nameOne}_{nameTwo}/{name}.txt");
             var tes = JsonConvert.DeserializeObject<Player>(txtInfo);
 
             return tes;
